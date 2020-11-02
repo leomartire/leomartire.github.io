b4c07c1d9fd4e5874f689ad4a7de6a657d951650
Léo Martire <30411754+leomartire@users.noreply.github.com>
2020-11-03 00:46:39 +0100

    [AUTO] Automatic update (2011030046).

--- produce.py
+++ produce.py
@@ -12,11 +12,12 @@ def produce_publications(input_file, output_file):
   fmt_a2 = [bibitem_print(a, 2) for a in a2]
   fmt_an = [bibitem_print(a, -1) for a in an]
   
-  join_a1 = '\n<br>\n'.join(fmt_a1)
-  join_a2 = '\n<br>\n'.join(fmt_a2)
-  join_an = '\n<br>\n'.join(fmt_an)
+  join_a1 = '  <li>'+('</li>\n  <li>'.join(fmt_a1))+'</li>'
+  join_a2 = '  <li>'+('</li>\n  <li>'.join(fmt_a2))+'</li>'
+  join_an = '  <li>'+('</li>\n  <li>'.join(fmt_an))+'</li>'
   
-  publications_str = join_a1+'\n<br><br>\n'+join_a2+'\n<br><br>\n'+join_an
+  publications_str = '<ul>\n'+join_a1+'\n</ul>\n\n<ul>\n'+join_a2+'\n</ul>\n\n<ul>\n'+join_an+'\n</ul>'
+  #print(publications_str); stop
   
   fid = open(output_file, 'w')
   fid.write(publications_str)
